47b7842643ff8b83c58487b6ce3230e99c03896b
Evellyn Lima <evellynlrl@hotmail.com>
2020-01-07 18:41:13 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+Esse projeto foi criado com o Create React App.
+
+Para instalar as dependências utilize o comando: npm install
+
+Para rodar o projeto utilize o comando: npm start
+
+Para acessar o projeto abra o link: [http://localhost:3000](http://localhost:3000)
+
+Para rodar os testes utilize o comando: npm test
---
+++ src/app/http.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const httpClient = axios.create({
+  baseURL: 'https://6nrr6n9l50.execute-api.us-east-1.amazonaws.com/default/front-plantTest-service'
+});
+
+const request = options => {
+  const onSuccess = response => response.data;
+
+  const onError = error => {
+    if (error.response.status === 500) {
+      throw error;
+    }
+
+    return Promise.reject(error);
+  };
+
+  return httpClient(options)
+    .then(onSuccess)
+    .catch(onError);
+};
+
+export default request;

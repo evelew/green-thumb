b69dc87c1c1b95d34105490119ce8b704dae5ae0
Evellyn Lima <evellynlrl@hotmail.com>
2020-01-17 12:10:58 -0300

    Vai ao topo da página quando muda de rota

--- src/common/browser-history.js
+++ src/common/browser-history.js
@@ -2,4 +2,10 @@ import { createBrowserHistory } from 'history'
 
 const history = createBrowserHistory()
 
+history.listen((location, action) => {
+  if (action === 'PUSH') {
+    window.scrollTo(0, 0)
+  }
+})
+
 export default history
